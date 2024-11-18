52428b546bc44b7f0e0051c64f217fc4774d97b7
arshadda <arshadda@ibm.com>
2024-11-18 11:39:25 +0530

    Add Chennai region and zones and update sysTypes

--- region_test.go
+++ region_test.go
@@ -116,6 +116,12 @@ func TestGetRegion(t *testing.T) {
 			"mad",
 			false,
 		},
+		{
+			"Chennai",
+			args{"che01"},
+			"che",
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
